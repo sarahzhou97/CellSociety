04694199e3affc33af1a6303cf1e111564ea44f4
Moses Wayne <msw38@duke.edu>
2017-01-31 17:48:30 -0500

    Created Abstract Class Simulation

---
+++ src/cellsociety_team10/Simulation.java
@@ -0,0 +1,12 @@
+package cellsociety_team10;
+
+public abstract class Simulation {
+	private int screenSizeX;
+	private int screenSizeY;
+	
+	public Simulation(int dimensionX, int dimensionY) {
+		screenSizeX = dimensionX;
+		screenSizeY = dimensionY;
+	}
+
+}
