5fe004e5754964e4555a987c16dff23be0b2fd38
Daniel <drl21@duke.edu>
2017-02-06 22:54:00 -0500

    s

--- src/Cells_Wator/WatorPredator.java
+++ src/Cells_Wator/WatorPredator.java
@@ -1,11 +1,15 @@
 package Cells_Wator;
 
+import javafx.scene.paint.Color;
+
 public class WatorPredator extends WatorCreature{
+	private final Color PREDATOR_COLOR=Color.RED;
 	private final int PREDATOR_GESTATION_PERIOD=10;
 	private final int MAX_HUNGER_PERIOD=6;
 	private int timeSinceAte=0;
 	
 	public WatorPredator(){
+		setColor(PREDATOR_COLOR);
 		setGestationPeriod(PREDATOR_GESTATION_PERIOD);
 	}
 	
